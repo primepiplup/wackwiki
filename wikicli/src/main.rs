c4ac8aa9d174dbe224b2f1712c387dd60dc6c94e
Daan Kuik <dkuik@sogyo.nl>
2024-03-05 17:05:54 +0100

    Basic subcommand structure set up

--- wikicli/src/main.rs
+++ wikicli/src/main.rs
@@ -1,3 +1,40 @@
+mod groups;
+mod new;
+mod dirs;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let wikipath = match env::var("WIKIPATH") {
+        Ok(path) => path,
+        Err(_)   => {
+            println!("WIKIPATH environment variable is undefined.");
+            return;
+        }
+    };
+
+    if args.len() < 2 {
+        print_help();
+    } else {
+        match args[1].as_str() {
+            "help" => print_help(),
+            "groups" => groups::handle(args, wikipath),
+            "new" => new::handle(args),
+            _ => {
+                println!("Unrecognized command.\n");
+                print_help();
+            }
+        }
+    }
 }
+
+fn print_help() -> () {
+    println!("Usage: ");
+    println!("wikicli [subcommand]");
+    println!("\nAvailable subcommands:");
+    println!("help   -- display this help message");
+    println!("groups -- manage groups and subgroups");
+    println!("new    -- create a new wiki entry");
+}
+
