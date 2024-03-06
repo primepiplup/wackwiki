42b4ea3d2469c9ecda8756f30b1819c436502deb
Daan Kuik <dkuik@sogyo.nl>
2024-03-06 16:50:28 +0100

    functions for managing articles

---
+++ wikicli/src/articles.rs
@@ -0,0 +1,60 @@
+use std::fs;
+
+pub fn post(path: String, entryname: &String, entrytext: String) -> () {
+    let mut content = "# ".to_owned();
+    content += entryname;
+    content += "\n\n";
+    content += &entrytext;
+    
+    let path = path + "/" + entryname;
+    match fs::write(path, content) {
+        Ok(_) => println!("Created new article: {}", entryname),
+        Err(_) => println!("Something went wrong in the creation of the article"),
+    };
+}
+
+pub fn list_articles(path: &String) -> Vec<String> {
+    let mut files: Vec<String> = Vec::new();
+    let file_iter = match fs::read_dir(path) {
+        Ok(files) => files,
+        Err(_)   => {
+            println!("Directory appears to be empty.");
+            return files;
+        }
+    };
+
+    for file in file_iter {
+        let file = match file {
+            Ok(file) => file,
+            Err(_) => {
+                println!("Something went wrong during directory scanning.");
+                return files;
+            }
+        };
+
+        if let Ok(file_type) = file.file_type() {
+            if !file_type.is_dir() {
+                let filename = match file.file_name().into_string() {
+                    Ok(dir_name) => dir_name,
+                    Err(_) => {
+                        println!("OS file name did not contain valid unicode");
+                        return files;
+                    }
+                };
+                files.push(filename);
+            }
+        }
+    };
+    return files;
+}
+
+pub fn exists(path: &String, entryname: &String) -> bool {
+    let articles = list_articles(path);
+
+    for article in &articles {
+        if article == entryname {
+            return true;
+        }
+    }
+    return false;
+}
