3f1d6346578bcb3d94e0cda6ec2861cba1b9829d
Daan Kuik <dkuik@sogyo.nl>
2024-03-06 16:51:55 +0100

    Groups are way cooler now and can deal with creating and removing subgroups

--- wikicli/src/groups.rs
+++ wikicli/src/groups.rs
@@ -19,7 +19,7 @@ fn print_help() -> () {
     println!("wikicli groups [subcommand]");
     println!("\nSubcommands:");
     println!("help   -- display this help message");
-    println!("list   -- output a list of all groups currently in the wiki");
+    println!("list   -- output a list of all groups currently in the wiki, or subgroups in a group");
     println!("new    -- create a new group or subgroup");
     println!("remove -- remove a group or subgroup");
 }
@@ -34,37 +34,57 @@ fn list_groups(wikipath: String) -> () {
 }
 
 fn create_group(wikipath: String, args: Vec<String>) -> () {
-    let dirs = dirs::get_dirs(&wikipath);
-
+    let mut path = wikipath;
     if args.len() < 4 {
         println!("Provide name for new group");
         return;
     }
 
-    let groupname = args[3].clone();
+    let mut groupname = args[3].clone();
+    if groupname.contains("/") {
+        let pathparts: Vec<&str> = groupname.split("/").collect();
+        let mut count = 0;
+        while count < pathparts.len() - 1 {
+            path = path + "/" + pathparts[count];
+            count += 1;
+        }
+        groupname = pathparts[pathparts.len() - 1].to_owned();
+    }
+
+    let dirs = dirs::get_dirs(&path);
     if dirs.contains(&groupname) {
         println!("Group already exists");
         return;
     }
 
-    dirs::create(wikipath, &groupname);
+    dirs::create(path, &groupname);
 }
 
 fn remove_group(wikipath: String, args: Vec<String>) -> () {
-    let dirs = dirs::get_dirs(&wikipath);
-
+    let mut path = wikipath;
     if args.len() < 4 {
-        println!("Provide name for the group to delete");
+        println!("Provide name for new group");
         return;
     }
 
-    let groupname = args[3].clone();
+    let mut groupname = args[3].clone();
+    if groupname.contains("/") {
+        let pathparts: Vec<&str> = groupname.split("/").collect();
+        let mut count = 0;
+        while count < pathparts.len() - 1 {
+            path = path + "/" + pathparts[count];
+            count += 1;
+        }
+        groupname = pathparts[pathparts.len() - 1].to_owned();
+    }
+
+    let dirs = dirs::get_dirs(&path);
     if !dirs.contains(&groupname) {
-        println!("Group does not exist");
+        println!("Group does not exist.");
         return;
     }
 
-    dirs::remove(wikipath, &groupname);
+    dirs::remove(path, &groupname);
 }
 
 fn print_vector(dirs: Vec<String>) -> () {
