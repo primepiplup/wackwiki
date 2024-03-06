073baa6ba4f54292d5884ebff5947ed4c39ec26b
Daan Kuik <dkuik@sogyo.nl>
2024-03-06 11:06:40 +0100

    Can report on whether directories exist, create and remove directories

--- wikicli/src/dirs.rs
+++ wikicli/src/dirs.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-pub fn get_dirs(wikipath: String) -> Vec<String> {
+pub fn get_dirs(wikipath: &String) -> Vec<String> {
     let mut dirs: Vec<String> = Vec::new();
     let dir_iter = match fs::read_dir(wikipath) {
         Ok(dirs) => dirs,
@@ -33,4 +33,39 @@ pub fn get_dirs(wikipath: String) -> Vec<String> {
         }
     };
     return dirs;
+}
+
+pub fn create(wikipath: String, name: &String) -> () {
+    let newpath = wikipath + "/" + name;
+    match fs::create_dir(newpath) {
+        Ok(_) => println!("Created new group: {}", name),
+        Err(_) => println!("Failed to create directory for new group"),
+    };
+}
+
+pub fn remove(wikipath: String, name: &String) -> () {
+    let removepath = wikipath + "/" + name;
+    match fs::remove_dir(removepath) {
+        Ok(_) => println!("Succesfully removed group: {}", name),
+        Err(_) => println!("Unable to remove group, something went wrong"),
+    }
+}
+
+pub fn contains(wikipath: &String, name: &String) -> bool {
+    let dirs = get_dirs(&wikipath);
+
+    for dir in &dirs {
+        if dir == name {
+            return true;
+        }
+    }
+
+    let pluralized = name.to_owned() + "s";
+    for dir in &dirs {
+        if dir == &pluralized {
+            return true;
+        }
+    }
+
+    return false;
 }
