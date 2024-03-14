175ba8b8a2ccbf825aba7b8c8963b176a9f8c17b
Daan Kuik <dkuik@sogyo.nl>
2024-03-14 11:15:57 +0100

    Tags split up into bold and italic, added token types

--- wikiserv/src/token.rs
+++ wikiserv/src/token.rs
@@ -1,11 +1,14 @@
+#[derive(Clone)]
 pub struct LiteralToken {
     content: String,
+    tokentype: TokenType,
 }
 
 impl LiteralToken {
     pub fn new(content: String) -> LiteralToken {
         LiteralToken {
-            content
+            content,
+            tokentype: TokenType::LITERAL,
         }
     }
 }
@@ -14,39 +17,92 @@ impl Token for LiteralToken {
     fn add(&self) -> String {
         return self.content.to_owned();
     }
+
+    fn tokentype(&self) -> &TokenType {
+        return &self.tokentype;
+    }
+
+    fn literal_replace(&self) -> Box<dyn Token> {
+        return Box::new(self.to_owned());
+    }
 }
 
-pub struct TagToken<'a> {
-    tag: &'a str,
+pub struct BoldToken {
     closer: bool,
+    tokentype: TokenType,
 }
 
-impl TagToken<'_> {
-    pub fn new(tag: &str, closer: bool) -> TagToken {
-        TagToken {
-            tag,
+impl BoldToken {
+    pub fn new(closer: bool) -> BoldToken {
+        BoldToken {
             closer,
+            tokentype: TokenType::BOLD,
         }
     }
 }
 
-impl Token for TagToken<'_> {
+impl Token for BoldToken {
     fn add(&self) -> String {
         if !self.closer {
-            return format!("<{}>", self.tag);
+            return format!("<b>");
         } else {
-            return format!("</{}>", self.tag);
+            return format!("</b>");
+        }
+    }
+
+    fn tokentype(&self) -> &TokenType {
+        return &self.tokentype;
+    }
+
+    fn literal_replace(&self) -> Box<dyn Token> {
+        return Box::new(CharToken::new('*'));
+    }
+}
+
+pub struct ItalicToken {
+    closer: bool,
+    tokentype: TokenType,
+}
+
+impl ItalicToken {
+    pub fn new(closer: bool) -> ItalicToken {
+        ItalicToken {
+            closer,
+            tokentype: TokenType::ITALIC,
         }
     }
 }
 
+impl Token for ItalicToken {
+    fn add(&self) -> String {
+        if !self.closer {
+            return format!("<i>");
+        } else {
+            return format!("</i>");
+        }
+    }
+
+    fn tokentype(&self) -> &TokenType {
+        return &self.tokentype;
+    }
+
+    fn literal_replace(&self) -> Box<dyn Token> {
+        return Box::new(CharToken::new('_'));
+    }
+}
+
+#[derive(Clone)]
 pub struct CharToken {
     c: char,
+    tokentype: TokenType,
 }
 
 impl CharToken {
     pub fn new(c: char) -> CharToken {
-        CharToken { c }
+        CharToken {
+            c,
+            tokentype: TokenType::CHAR,
+        }
     }
 }
 
@@ -54,8 +110,26 @@ impl Token for CharToken {
     fn add(&self) -> String {
         return self.c.to_string();
     }
+
+    fn tokentype(&self) -> &TokenType {
+        return &self.tokentype;
+    }
+
+    fn literal_replace(&self) -> Box<dyn Token> {
+        return Box::new(self.to_owned());
+    }
 }
 
 pub trait Token {
     fn add(&self) -> String;
+    fn tokentype(&self) -> &TokenType;
+    fn literal_replace(&self) -> Box<dyn Token>;
+}
+
+#[derive(PartialEq, Clone)]
+pub enum TokenType {
+    BOLD,
+    CHAR,
+    ITALIC,
+    LITERAL,
 }
