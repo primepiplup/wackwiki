9443c38ed42d66fc9df0a21d1f68f7a3e558cd0a
Daan Kuik <dkuik@sogyo.nl>
2024-03-20 10:00:52 +0100

    Images module to return image data

---
+++ wikiserv/src/images.rs
@@ -0,0 +1,10 @@
+use crate::Paths;
+
+pub fn get_image(paths: &Paths, requestpath: &str) -> Result<Vec<u8>, ()> {
+    let path = paths.wikipath().to_owned() + "/.images/" + requestpath;
+    println!("searching for image at: {}", path);
+    match std::fs::read(path) {
+        Ok(image_data) => Ok(image_data),
+        Err(_)         => return Err(()),
+    }
+}
