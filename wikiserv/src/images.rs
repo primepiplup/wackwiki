524dc6a707f3f0b6add2a159374d5799691b093a
Daan Kuik <dkuik@sogyo.nl>
2024-03-20 15:06:59 +0100

    Know that links are converted to /.images/ inclusive

--- wikiserv/src/images.rs
+++ wikiserv/src/images.rs
@@ -1,7 +1,7 @@
 use crate::Paths;
 
 pub fn get_image(paths: &Paths, requestpath: &str) -> Result<Vec<u8>, ()> {
-    let path = paths.wikipath().to_owned() + "/.images/" + requestpath;
+    let path = paths.wikipath().to_owned() + requestpath;
     println!("searching for image at: {}", path);
     match std::fs::read(path) {
         Ok(image_data) => Ok(image_data),
