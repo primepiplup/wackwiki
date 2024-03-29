ba0437abea815014bf8fbf998c34d682f694a28b
Daan Kuik <dkuik@sogyo.nl>
2024-03-29 11:52:57 +0100

    Replaced all building of html responses with calls to http response builders

--- wikiserv/src/main.rs
+++ wikiserv/src/main.rs
@@ -1,4 +1,5 @@
 mod content;
+mod http;
 mod parser;
 mod paths;
 mod post;
@@ -153,7 +154,7 @@ fn handle_connection(mut stream: TcpStream, paths: &Paths) -> () {
     } else if method == "POST" {
         response = post_response(paths, &requestpath, body);
     } else {
-        response = fourhundred();
+        response = http::basic_response(400) ;
     }
 
     match stream.write_all(&response) {
@@ -166,67 +167,50 @@ fn handle_connection(mut stream: TcpStream, paths: &Paths) -> () {
 }
 
 fn post_response(paths: &Paths, requestpath: &str, body: String) -> Vec<u8> {
-    let status = "HTTP/1.1 200 OK";
     let headers = "";
 
     let (command, command_info) = match body.split_once(":") {
         Some(split) => split,
         None        => {
             println!("Malformed body within POST request. Returning 400.");
-            return fourhundred();
+            return http::basic_response(400);
         }
     };
 
-    let content = match post::handle(paths, requestpath, command, command_info) {
+    let mut content = match post::handle(paths, requestpath, command, command_info) {
         Ok(content) => content,
         Err(_)      => {
             println!("");
-            return fourhundred();
+            return http::basic_response(400);
         }
     };
 
-    let length = content.len();
-    
-    let html_head = format!("{status}\r\nContent-Length: {length}\r\n{headers}\r\n").as_bytes().to_vec();
-    return html_head;
+    return http::response(200, headers.to_string(), &mut content);
 }
 
 fn get_response(paths: &Paths, requestpath: &str) -> Vec<u8> {
-    let status = "HTTP/1.1 200 OK";
     let mut headers = "";
     let mut content: Vec<u8>;
 
     if requestpath.ends_with(".jpg")  {
         content = match images::get_image(paths, requestpath) {
             Ok(content) => content,
-            Err(_)      => return four_oh_four(),
+            Err(_)      => return http::basic_response(404),
         };
         headers = "Content-Type: image/jpeg\r\n";
     } else if requestpath.ends_with(".png") {
         content = match images::get_image(paths, requestpath) {
             Ok(content) => content,
-            Err(_)      => return four_oh_four(),
+            Err(_)      => return http::basic_response(404),
         };
         headers = "Content-Type: image/png\r\n";
     } else {
         content = match content::get_html(paths, requestpath) {
             Ok(content) => content,
-            Err(_)      => return four_oh_four(),
+            Err(_)      => return http::basic_response(404),
         };
     }
 
-    let length = content.len();
-
-    let mut html_head = format!("{status}\r\nContent-Length: {length}\r\n{headers}\r\n").as_bytes().to_vec();
-
-    html_head.append(&mut content);
-    return html_head;
+    return http::response(200, headers.to_string(), &mut content);
 }
 
-fn four_oh_four() -> Vec<u8> {
-    return "HTTP/1.1 404 Not Found\r\n\r\n".as_bytes().to_vec();
-}
-
-fn fourhundred() -> Vec<u8> {
-    return "HTTP/1.1 400 Bad Request\r\n\r\n".as_bytes().to_vec();
-}
