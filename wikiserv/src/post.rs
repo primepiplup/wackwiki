8a87bbd4ce237038c9a6facd8f542583c51b0a4a
Daan Kuik <dkuik@sogyo.nl>
2024-04-03 11:21:07 +0200

    POST handler now sends specific error status codes based on what goes wrong

--- wikiserv/src/post.rs
+++ wikiserv/src/post.rs
@@ -2,24 +2,24 @@ use crate::paths::Paths;
 use std::io::{prelude::*, BufReader, BufWriter};
 use std::fs::{File, self};
 
-pub fn handle(paths: &Paths, requestpath: &str, command: &str, command_info: &str) -> Result<Vec<u8>, ()> {
+pub fn handle(paths: &Paths, requestpath: &str, command: &str, command_info: &str) -> Result<Vec<u8>, usize> {
     match command {
         "checkbox" => checkbox(paths, requestpath, command_info),
-        _ => Err(()),
+        _ => Err(400),
     }
 }
 
-fn checkbox(paths: &Paths, requestpath: &str, info: &str) -> Result<Vec<u8>, ()> {
+fn checkbox(paths: &Paths, requestpath: &str, info: &str) -> Result<Vec<u8>, usize> {
     let (line_number, column_number) = match info.split_once("|") {
         Some(split) => split,
-        None => return Err(()),
+        None => return Err(400),
     };
 
     let line_number: usize = match line_number.trim().parse() {
         Ok(num) => num,
         Err(_)  => {
             println!("Failed to parse line number. Returning 400.");
-            return Err(());
+            return Err(400);
         },
     };
 
@@ -27,7 +27,7 @@ fn checkbox(paths: &Paths, requestpath: &str, info: &str) -> Result<Vec<u8>, ()>
         Ok(num) => num,
         Err(_)  => {
             println!("Failed to parse column number. Returning 400.");
-            return Err(());
+            return Err(400);
         },
     };
 
@@ -38,7 +38,7 @@ fn checkbox(paths: &Paths, requestpath: &str, info: &str) -> Result<Vec<u8>, ()>
         Ok(file) => file,
         Err(_)   => {
             println!("Was unable to open file mentioned in POST request.");
-            return Err(());
+            return Err(400);
         }
     };
 
@@ -46,7 +46,7 @@ fn checkbox(paths: &Paths, requestpath: &str, info: &str) -> Result<Vec<u8>, ()>
         Ok(file) => file,
         Err(_)   => {
             println!("Could not create temporary file to write new information to");
-            return Err(());
+            return Err(500);
         },
     };
 
@@ -60,7 +60,7 @@ fn checkbox(paths: &Paths, requestpath: &str, info: &str) -> Result<Vec<u8>, ()>
             Ok(line) => line,
             Err(_)   => {
                 println!("Something went wrong when reading the wiki file.");
-                return Err(());
+                return Err(500);
             },
         };
 
@@ -69,7 +69,7 @@ fn checkbox(paths: &Paths, requestpath: &str, info: &str) -> Result<Vec<u8>, ()>
             let boxpos = column_number - 1;
             if linechars[boxpos - 1] != '[' && linechars[boxpos + 1] != ']' {
                 println!("Incorrect position given for checkbox.");
-                return Err(());
+                return Err(400);
             }
             if linechars[boxpos] == ' ' {
                 linechars[boxpos] = 'X';
@@ -85,7 +85,7 @@ fn checkbox(paths: &Paths, requestpath: &str, info: &str) -> Result<Vec<u8>, ()>
             Ok(_)  => (),
             Err(_) => {
                 println!("Was unable to write to temporary file. Exiting.");
-                return Err(());
+                return Err(500);
             }
         };
     }
@@ -94,7 +94,7 @@ fn checkbox(paths: &Paths, requestpath: &str, info: &str) -> Result<Vec<u8>, ()>
         Ok(_)  => (),
         Err(_) => {
             println!("Was unable to replace old file with new temporary file.");
-            return Err(());
+            return Err(500);
         }
     }
 
